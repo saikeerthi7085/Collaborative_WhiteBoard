3125521c35bc0557673dc3bf23e7994f8086e32a
saikeerthi7085 <saikeerthimaddineni@gmail.com>
2020-12-12 09:00:38 +0000

    Add project files.

---
+++ Collaborative_WhiteBoard/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Collaborative_WhiteBoard.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
---
+++ Collaborative_WhiteBoard/Startup.cs
@@ -0,0 +1,45 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.HttpsPolicy;
+
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+namespace Microsoft.Azure.SignalR.Samples.Whiteboard
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddSingleton<Diagram>();
+            services.AddMvc();
+            services.AddSignalR();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app)
+        {
+            app.UseRouting();
+            app.UseFileServer();
+            app.UseEndpoints(routes =>
+            {
+                routes.MapHub<DrawHub>("/draw");
+            });
+        }
+    }
+}
